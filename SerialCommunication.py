54bfcc8bddbe368e259f662a1ac91721595b5d7c
lukkascost <lucas.costa@lit.ifce.edu.br>
2018-06-22 15:30:57 -0300

    meter simulation response

--- SerialCommunication.py
+++ SerialCommunication.py
@@ -2,12 +2,12 @@ import serial as sr
 from Commands.AT_Commands import *
 
 serRead = sr.Serial(
-    port='/dev/tty.usbserial-00002014A',
+    # port='/dev/tty.usbserial-00002014A',
+    port='/dev/tty.usbserial',
     baudrate=9600,
     timeout=10
 )
 
 comando = At_command("AT+NBAND?", serRead)
-for i in comando.execute_command():
-    print i
-
+print comando.get_bytes_from_n_m_g_r()
+comando.send_to_meter([],[])
